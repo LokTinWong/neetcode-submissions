b6968f6707ab50c4039b58409b8721c67669c436
Lok Tin Wong <loktinw0620@gmail.com>
2026-05-15 21:21:54 -0700

    Added complexity results

--- Data Structures & Algorithms/validate-parentheses/submission-3.java
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -1,20 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-    Deque<Character> stack = new ArrayDeque<>();
-
-    for (char c : s.toCharArray()) {
-        if (c == '(') {
-            stack.push(')');
-        } else if (c == '{') {
-            stack.push('}');  
-        } else if (c == '[') {
-            stack.push(']');
-        } else {
-            if (stack.isEmpty() || stack.pop() != c) {
-                return false;
+        // Runtime: Beats 37.86%
+        // Memory: Beats 45.57%
+        // It looks so much cleaner than submission-2, but runtime is not as good.
+        
+        Deque<Character> stack = new ArrayDeque<>();
+    
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');  
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
             }
         }
-    }
-        return stack.isEmpty();
-    }
+            return stack.isEmpty();
+        }
 }
