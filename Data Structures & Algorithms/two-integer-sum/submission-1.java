af997f1bd0f4beb2294a09f24068219e436a3020
Lok Tin Wong <loktinw0620@gmail.com>
2026-05-16 18:40:37 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] array = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                array[0] = map.get(nums[i]);
+                array[1] = i;
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+
+        return array;
+    }
+}
